c180c522e0107ee8ce48644b9214cbaaa938a3e6
Vadas István <vadas.istvan.1@gmail.com>
2020-12-12 21:40:46 +0100

    checkIndex function

--- todo.js
+++ todo.js
@@ -12,6 +12,7 @@ try {
     if (fs.existsSync(fileName)) {
         todoList = JSON.parse(fs.readFileSync(fileName, 'utf8'));
     } else {
+        console.log("Létre lett hozva, az eddig nem létező adat file")
         fs.writeFileSync(fileName, '[]');
     }
 
@@ -21,9 +22,9 @@ try {
     } else if (args.a) {
         addNewElement(args.a)
     } else if (args.r) {
-        removeElement(args.r,todoList)
+        removeElement(args.r, todoList)
     } else if (args.c) {
-        completeElement(args.c)
+        completeElement(args.c, todoList)
     } else if (Object.keys(args).length > 0) {
         console.log("Nem támogatott argumentum!");
         printUserManual()
@@ -47,6 +48,7 @@ function printUserManual() {
     forPrint.forEach(element => console.log(element));
 };
 
+
 function printToDoList(todoList) {
     let nothingTodo = true
     for (let i = 0; i < todoList.length; i++) {
@@ -58,6 +60,7 @@ function printToDoList(todoList) {
     }
 }
 
+
 function addNewElement(newElement) {
     if (typeof newElement === "boolean") {
         console.log("Nem lehetséges új feladat hozzáadása: nincs megadva a feladat!");
@@ -67,26 +70,41 @@ function addNewElement(newElement) {
     }
 }
 
-function removeElement(removeElement,todoList){
-    if (typeof removeElement === "boolean"){
-        console.log("Nem lehetséges az eltávolítás: nem adott meg indexet!");
-    }else if (typeof removeElement !== "number"){
-        console.log("Nem lehetséges az eltávolítás: a megadott index nem szám!");
-    }else if(todoList.length <= 0){
-        console.log("A tudu lista nem tartalmaz elemet, nincs mit törölni!");
-    }else if(todoList.length < removeElement){
-        console.log("Nem lehetséges az eltávolítás: túlindexelési probléma adódott!");
-    }else{
+
+function removeElement(removeElement, todoList) {
+    let errorText = "eltávolítás";
+    if (checkIndex(removeElement, errorText, todoList)) {
         todoList.splice((removeElement - 1), 1);
         //reindex
         for (let i = 0; i < todoList.length; i++) {
-            todoList[i].id = i+1
+            todoList[i].id = i + 1
         }
         //writing
-        fs.writeFileSync(fileName, JSON.stringify(todoList, null,2));
-
+        fs.writeFileSync(fileName, JSON.stringify(todoList, null, 2));
     }
 };
 
-function completeElement(complElement) {
+
+function completeElement(complElement, todoList) {
+    let errorText = "elvégzettre állítás";
+    if (checkIndex(complElement, errorText, todoList)) {
+        todoList[complElement - 1].done = true
+        fs.writeFileSync(fileName, JSON.stringify(todoList, null, 2));
+    }
+};
+
+
+function checkIndex(indexNumber, errorText, todoList) {
+    if (typeof indexNumber === "boolean") {
+        console.log(` Nem lehetséges az ${errorText}: nem adott meg indexet!`);
+    } else if (typeof indexNumber !== "number") {
+        console.log(`Nem lehetséges az ${errorText}: a megadott index nem szám!`);
+    } else if (todoList.length <= 0) {
+        console.log(`A tudu lista nem tartalmaz elemet, nincs lehetőség ${errorText}ra!`);
+    } else if (todoList.length < indexNumber) {
+        console.log(`Nem lehetséges az ${errorText}: túlindexelési probléma adódott!`);
+    } else {
+        return true
+    }
+    return false
 };
