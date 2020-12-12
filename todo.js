2f8bcb924d8d263e6e7ba2f58869257c3e2ad7f4
Vadas István <vadas.istvan.1@gmail.com>
2020-12-12 14:54:57 +0100

    Print user manual (if any parameter)

---
+++ todo.js
@@ -0,0 +1,57 @@
+'use strict';
+import fs from 'fs';
+import minimist from 'minimist';
+
+const args = minimist(process.argv);
+let fileName = "data/todoData.json"
+let todoList = [];
+
+try {
+
+    //Read data from todoData.json
+    if (fs.existsSync(fileName)) {
+        todoList = JSON.parse(fs.readFileSync(fileName, 'utf8'));
+    } else {
+        fs.writeFileSync(fileName, '[]');
+    }
+
+    //Main menu
+    if (args.l){
+        printToDoList(todoList);
+    }else if(args.a){
+        addNewElement(args.a)    
+    }else if(args.r){
+        removeElement(args.r)
+    }else if(args.c){
+        completeElement(args.c)
+    }else{
+        printUserManual()
+    }
+} catch (err) {
+    console.error(err);
+}
+
+
+function printUserManual() {
+    const forPrint = [
+        "Parancssori Todo applikáció",
+        "=============================",
+        "  ",
+        "-l   Kilistázza a feladatokat (list)",
+        "-a   Új feladatot ad hozzá (add)",
+        "-r   Eltávolít egy feladatot (remove)",
+        "-c   Teljesít egy feladatot (complete)"]
+    forPrint.forEach(element => console.log(element));
+};
+
+function printToDoList(todoList) {
+}
+
+function addNewElement(newElement){
+}
+
+function removeElement(removeElement){
+};
+
+function completeElement(complElement){
+};
